274aeb5805f7bbae343d9ed19eafe2da820f5192
John Casey <jdcasey@commonjava.org>
2018-07-30 22:40:54 -0500

    use README for long_description, other minor fixups

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@
 from setuptools import setup, find_packages
 import sys
 
+with open('README.rst', 'r') as f:
+  long_description=r.read()
+
 version='0.0.1'
 
 setup(
     zip_safe=True,
     name='carbon-slack',
     version=version,
-    long_description="Library for using Slack as a bus for sending data to GraphiteDB / Carbon",
+    description="Use Slack as a bus for GraphiteDB / Carbon",
+    long_description=long_description,
     classifiers=[
       "Development Status :: 3 - Alpha",
       "Intended Audience :: Developers",
@@ -17,7 +21,7 @@ setup(
       "Programming Language :: Python :: 2",
       "Topic :: Utilities",
     ],
-    keywords='graphite carbon metrics monitoring ',
+    keywords='graphite carbon metrics monitoring slack',
     author='John Casey',
     author_email='jdcasey@commonjava.org',
     url='https://github.com/jdcasey/carbon-slack',
